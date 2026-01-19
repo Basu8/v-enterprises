747420014ae1d60ddb0b86b1a6dd8444c0504e95
Basu8 <dasanis17@gmail.com>
2026-01-20 00:13:38 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,3 @@
-// // @ts-ignore
-// import "./globals.css";
-// import type { ReactNode } from "react";
-
-// import Navbar from "../components/Navbar";
-// import Footer from "../components/Footer";
-// import ChatbotTrigger from "../components/Chatbot/ChatbotTrigger";
-
-// export const metadata = {
-//   title: "V Enterprises | Industrial Hardware",
-//   description: "Premium industrial hardware solutions for modern manufacturing.",
-// };
-
-// export default function RootLayout({ children }: { children: ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <body className="bg-white text-black relative">
-//         <Navbar />
-//         <main>{children}</main>
-//         <Footer />
-//         <ChatbotTrigger />
-//       </body>
-//     </html>
-//   );
-// }
-
-// app/layout.tsx
 import "./globals.css";
 import type { ReactNode } from "react";
 import Navbar from "../components/Navbar";
@@ -34,6 +7,11 @@ import ChatbotTrigger from "../components/Chatbot/ChatbotTrigger";
 export const metadata = {
   title: "V Enterprises | Industrial Hardware",
   description: "Premium industrial hardware solutions for modern manufacturing.",
+   icons: {
+    icon: "/favicon.ico",       // browser + Google
+    // shortcut: "/favicon.ico",
+    // apple: "/apple-touch-icon.png", // optional
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
